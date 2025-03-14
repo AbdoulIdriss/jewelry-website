1e35df68d4012104f2f4b1cb1255f78237dd78c6
abdoulben44 <abdoulben44@gmail.com>
2025-03-14 14:28:05 +0000

    modified the heading

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myapp' title`, () => {
+  it(`should have the 'Jewelry' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Jewelry');
